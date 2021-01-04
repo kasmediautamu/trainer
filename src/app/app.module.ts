2308197f2c53133381d21504142c9e513ba71a94
kasmediautamu <ivan.khabuyhe1@gmail.com>
2021-01-04 23:02:06 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WorkoutRunnerComponent } from '../components/workout-runner/workout-runner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WorkoutRunnerComponent
   ],
   imports: [
     BrowserModule,
